533e7249db75aa4851b59de97a0afec1fd8a1355
Proseken <66033203+Proseken@users.noreply.github.com>
2025-01-27 06:32:07 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,59 @@
 #!/usr/bin/node
 
 const request = require('request');
-const filmUrl = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
-request(filmUrl, (error, response, body) => {
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error('Usage: ./0-starwars_characters.js <movieId>');
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
   if (error) {
-    return;
+    console.error('Error:', error);
+    process.exit(1);
   }
 
-  const film = JSON.parse(body);
-  const urlToName = {};
-  film.characters.forEach((characterUrl) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        return;
-      }
-      const character = JSON.parse(body);
-      urlToName[character.url] = character.name;
-      if (Object.keys(urlToName).length === film.characters.length) {
-        film.characters.forEach((characterUrl) =>
-          console.log(urlToName[characterUrl])
-        );
-      }
+  if (response.statusCode !== 200) {
+    console.error('Invalid response:', response.statusCode);
+    process.exit(1);
+  }
+
+  const filmData = JSON.parse(body);
+  const characters = filmData.characters;
+
+  if (!characters || characters.length === 0) {
+    console.error('No characters found for this movie.');
+    process.exit(1);
+  }
+
+  const fetchCharacterData = (characterUrl) => {
+    return new Promise((resolve, reject) => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else if (response.statusCode !== 200) {
+          reject(new Error(`Invalid response for character data: ${response.statusCode}`));
+        } else {
+          const characterData = JSON.parse(body);
+          resolve(characterData.name);
+        }
+      });
     });
-  });
+  };
+
+  // Use Promise.all to fetch all character data concurrently
+  Promise.all(characters.map(fetchCharacterData))
+    .then(characterNames => {
+      characterNames.forEach(name => {
+        console.log(name);
+      });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      process.exit(1);
+    });
 });
